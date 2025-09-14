aee0e5e4b6a28c58dc0cf16db8e064e0cfe77a50
AminPourabazari <abzery.ap@gmail.com>
2025-09-14 21:01:27 +0330

    Update NativeAdPayload.dart

--- lib/NativeAdPayload.dart
+++ lib/NativeAdPayload.dart
@@ -1,21 +1,19 @@
-// import 'package:google_mobile_ads/google_mobile_ads.dart';
-
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'NativeAdData.dart';
 
-///
-/// NativeAdPayload util class, used to handle different states of NativeAd (general, admob)
-///
-// abstract class NativeAdPayload {
-//   NativeAdPayload._();
+// NativeAdPayload util class, used to handle different states of NativeAd (general, admob)
 
-//   factory NativeAdPayload.general(NativeAdData ad) =>
-//       GeneralNativeAdPayload(ad);
+abstract class NativeAdPayload {
+  NativeAdPayload._();
 
-//   factory NativeAdPayload.adMob(Ad ad) => AdMobNativeAdPayload(ad);
+  factory NativeAdPayload.general(NativeAdData ad) =>
+      GeneralNativeAdPayload(ad);
 
-//   factory NativeAdPayload.adMobView(NativeAd nativeAd) =>
-//       AdMobNativeAdViewPayload(nativeAd);
-// }
+  factory NativeAdPayload.adMob(Ad ad) => AdMobNativeAdPayload(ad);
+
+  factory NativeAdPayload.adMobView(NativeAd nativeAd) =>
+      AdMobNativeAdViewPayload(nativeAd);
+}
 
 class GeneralNativeAdPayload extends NativeAdPayload {
   NativeAdData ad;
@@ -23,14 +21,14 @@ class GeneralNativeAdPayload extends NativeAdPayload {
   GeneralNativeAdPayload(this.ad) : super._();
 }
 
-// class AdMobNativeAdPayload extends NativeAdPayload {
-//   Ad ad;
+class AdMobNativeAdPayload extends NativeAdPayload {
+  Ad ad;
 
-//   AdMobNativeAdPayload(this.ad) : super._();
-// }
+  AdMobNativeAdPayload(this.ad) : super._();
+}
 
-// class AdMobNativeAdViewPayload extends NativeAdPayload {
-//   NativeAd nativeAdView;
+class AdMobNativeAdViewPayload extends NativeAdPayload {
+  NativeAd nativeAdView;
 
-//   AdMobNativeAdViewPayload(this.nativeAdView) : super._();
-// }
+  AdMobNativeAdViewPayload(this.nativeAdView) : super._();
+}
